5ce1051172b1bb4bfc145a2da3e12d4c488547e0
Ishan Verma <ishaniv123@gmail.com>
2019-02-18 13:33:13 -0500

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,116 @@
+import java.util.LinkedList;
+import java.io.*; 
+import java.util.*; 
+
+public class Graph {
+    int vertex;
+    LinkedList<Integer> list[];
+
+    public Graph(int vertex) {
+        this.vertex = vertex;
+        list = new LinkedList[vertex];
+        for (int i = 0; i <vertex ; i++) {
+            list[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination){
+
+        //add edge
+        list[source].addFirst(destination);
+
+        //add back edge ((for undirected)
+        list[destination].addFirst(source);
+    }
+
+    public void printGraph(){
+        for (int i = 0; i <vertex ; i++) {
+            if(list[i].size()>0) {
+                System.out.print("Vertex " + i + " is connected to: ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    void BFS(int s) 
+    { 
+        // Mark all the vertices as not visited(By default 
+        // set as false) 
+        boolean visited[] = new boolean[vertex]; 
+  
+        // Create a queue for BFS 
+        LinkedList<Integer> queue = new LinkedList<Integer>(); 
+  
+        // Mark the current node as visited and enqueue it 
+        visited[s]=true; 
+        queue.add(s); 
+  
+        while (queue.size() != 0) 
+        { 
+            // Dequeue a vertex from queue and print it 
+            s = queue.poll(); 
+            System.out.print(s+" "); 
+  
+            // Get all adjacent vertices of the dequeued vertex s 
+            // If a adjacent has not been visited, then mark it 
+            // visited and enqueue it 
+            Iterator<Integer> i = list[s].listIterator(); 
+            while (i.hasNext()) 
+            { 
+                int n = i.next(); 
+                if (!visited[n]) 
+                { 
+                    visited[n] = true; 
+                    queue.add(n); 
+                } 
+            } 
+        } 
+        
+        
+    } 
+    void DFSUtil(int v,boolean visited[]) 
+    { 
+        // Mark the current node as visited and print it 
+        visited[v] = true; 
+        System.out.print(v+" "); 
+  
+        // Recur for all the vertices adjacent to this vertex 
+        Iterator<Integer> i = list[v].listIterator(); 
+        while (i.hasNext()) 
+        { 
+            int n = i.next(); 
+            if (!visited[n]) 
+                DFSUtil(n, visited); 
+        } 
+    } 
+  
+    // The function to do DFS traversal. It uses recursive DFSUtil() 
+    void DFS(int v) 
+    { 
+        // Mark all the vertices as not visited(set as 
+        // false by default in java) 
+        boolean visited[] = new boolean[vertex]; 
+  
+        // Call the recursive helper function to print DFS traversal 
+        DFSUtil(v, visited); 
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0,1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
+        graph.BFS(0);
+        System.out.println();
+        graph.DFS(0);
+        
+        
+    }
+}
